c8d38f0292902afd3d694b718d2ef5bdb92d26a2
aprilkrg <aprilkrgonzales@gmail.com>
2022-12-14 10:46:03 -0700

    complete part 1

--- index.js
+++ index.js
@@ -1,97 +1,71 @@
-// require express package and invoke
 const express = require("express")
 const app = express()
-// create port variable
 const PORT = 3000
-// configuration ^^^ (THIS SHOULD ALWAYS BE BEFORE THE ROUTES)
 
-// data created from the zodiac.jpeg
-// a skill to work on is looking at information, organizing it, and translating it to usable javascript data 
-// for `elements` I could either use an array or an object. I started with an array, but then thinking about how I was gonna pass the data to the `res.send` I wanted to be able to say something simple like `elements.air` so I could be sure what I was getting, instead of using an index number. 
 const elements = {
-    air: { 
-        type: "air",
-        signs: ["Aquarius", "Gemini", "Libra"], 
-        traits: ["movement", "creativity", "action", "adventure", "exciting", "easily provoked"]
+    air: {
+        signs: ['aquarius', 'gemini', 'libra'],
+        traits: ["movement", "vcreativity", "action"]
     },
     water: {
-        type: "water",
-        signs: ["Pisces", "Cancer", "Scorpio"], 
-        traits: ["private", "mysterious", "pyshic", "charming", "emotional", "sensitive"]
+        signs: ['pisces', 'cancer', 'scorpio'],
+        traits: ['private', 'mysterious', 'pyshic']
     },
     fire: {
-        type: "fire",
-        signs: ["Aries", "Leo", "Sagittarius"], 
-        traits: ["passionate", "strong emotions", "tempermental", "energetic", "accomplished", "interesting"]
+        signs: ['aries', 'leo', 'sagittarius'],
+        traits: ['passionate', 'strong emtions', 'tempermental']
     },
     earth: {
-        type: "earth",
-        signs: ["Taurus", "Virgo", "Capricorn"], 
-        traits: ["grounded", "helpful", "practical", "realistic", "materialistic", "dependable"]
+        signs: ['taurus', 'virgo', 'capricorn'],
+        traits: ['grounded', 'helpful', 'practical']
     }
 }
-// HOME ROUTE
-// HTTP method = GET
-// URL pattern = "localhost:3000/"
-app.get("/", (req, res) => {
-    // // ROUTE CHECK
-    // res.send("HOME PAGE")
-    // // SEND FILE
-    res.sendFile(__dirname+'/views/index.html');
-    // // SEND VIEW
-    // res.render("index.ejs")
-})
 
-// AIR ROUTE
-// HTTP method = GET
-// URL pattern = "localhost:3000/air"
-app.get("/air", (req, res) => {
-    // // ROUTE CHECK
-    // res.send("AIR")
-    // // SEND DATA
-    // res.json(elements.air)
-    // // SEND VIEW
-    res.render("show.ejs", {element: elements.air})
+// routes
+// full url is localhost:3000/
+// GET /
+app.get("/", function(req, res) {
+    // check the route
+    // res.send("home page")
+    // render
+    res.render("index.ejs")
 })
-
-// WATER ROUTE
-// HTTP method = GET
-// URL pattern = "localhost:3000/water"
-app.get("/water", (req, res) => {
-    // // ROUTE CHECK
+// GET /water
+app.get("/water", function(req, res) {
     // res.send("WATER")
-    // // SEND DATA
-    // res.json(elements.water)
-    // // SEND VIEW
-    res.render("show.ejs", {element: elements.water})
+    // check the data
+    // res.send(elements.water.traits)
+    // rendering data with view
+    const signs = elements.water.signs
+    const traits = elements.water.traits
+    res.render("show.ejs", {signs:signs, traits:traits})
 })
-
-// EARTH ROUTE
-// HTTP method = GET
-// URL pattern = "localhost:3000/earth"
-app.get("/earth", (req, res) => {
-    // // ROUTE CHECK
-    // res.send("EARTH")
-    // // SEND DATA
-    // res.json(elements.earth)
-    // // SEND VIEW
-    res.render("show.ejs", {element: elements.earth})
+// GET /air
+app.get("/air", function(req, res) {
+    // res.send("AIR")
+    // rendering data with view
+    const signs = elements.air.signs
+    const traits = elements.air.traits
+    res.render("show.ejs", {signs:signs, traits:traits})    
 })
-
-// FIRE ROUTE
-// HTTP method = GET
-// URL pattern = "localhost:3000/fire"
-app.get("/fire", (req, res) => {
-    // // ROUTE CHECK
+// GET /fire
+app.get("/fire", function(req, res) {
     // res.send("FIRE")
-    // // SEND DATA
-    // res.json(elements.fire)
-    // // SEND VIEW
-    res.render("show.ejs", {element: elements.fire})
+    // rendering data with view
+    const signs = elements.fire.signs
+    const traits = elements.fire.traits
+    res.render("show.ejs", {signs:signs, traits:traits})  
+})
+// GET /earth
+app.get("/earth", function(req, res) {
+    // res.send("EARTH")
+    // rendering data with view
+    const signs = elements.earth.signs
+    const traits = elements.earth.traits
+    res.render("show.ejs", {signs:signs, traits:traits})  
 })
 
 
-// create listener (THIS SHOULD ALWAYS BE AT THE BOTTOM)
-app.listen(PORT, () => {
-    console.log(`Server live and listening on port ${PORT}`)
+app.listen(PORT, function(){
+    console.log("server is running on port " + PORT)
 })
